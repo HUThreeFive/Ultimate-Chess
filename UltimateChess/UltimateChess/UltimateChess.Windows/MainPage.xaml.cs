9c03b314ffa7a5d35d56d1becfb91741addce613
HUThreeFive <awebb@harding.edu>
2015-11-17 16:10:16 -0600

    Worked on basic UI layout

--- UltimateChess/UltimateChess/UltimateChess.Windows/MainPage.xaml.cs
+++ UltimateChess/UltimateChess/UltimateChess.Windows/MainPage.xaml.cs
@@ -24,12 +24,38 @@ namespace UltimateChess
     {
         public Coordinate firstClick;
         public Coordinate secondClick;
+        private double canvasSize;
 
         public MainPage()
         {
             this.InitializeComponent();
             GridModel Grid = new GridModel();
             Grid.Start();
+
+            if(Window.Current.Bounds.Height < Window.Current.Bounds.Width)
+            {
+                canvasSize = Window.Current.Bounds.Height - 128;
+            }
+        }
+
+        private void btnPlay_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnColor_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnHelp_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnExit_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
